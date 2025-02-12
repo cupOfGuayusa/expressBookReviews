e7c419fbbab68b2948f97ef8ed03c546730591e4
cupOfGuayusa <cupOfGuayusa@gmail.com>
2025-02-11 19:43:08 -0600

    Removed Incorrect Code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -9,28 +9,6 @@ const public_users = express.Router();
 public_users.post("/register", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-
-  if (!username && !password) {
-    return res.status(404).json({ message: "Error logging in." });
-  }
-  if (authenticatedUser(username, password)) {
-    let accessToken = jwt.sign(
-      {
-        data: password,
-      },
-      "access",
-      { expiresIn: 60 * 60 }
-    );
-    req.session.authorization = {
-      accessToken,
-      username,
-    };
-    return res.status(200).send("User successfully logged in!");
-  } else {
-    return res
-      .status(208)
-      .json({ message: "Invalid Login. Check username and password" });
-  }
 });
 
 // Get the book list available in the shop
